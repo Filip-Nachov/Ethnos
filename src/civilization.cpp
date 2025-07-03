529c4263d6a2bf80e6617e54276bf0b49193a727
Filip Nachov <fnachov@gmail.com>
2025-07-03 13:15:25 +0200

    changes

--- src/civilization.cpp
+++ src/civilization.cpp
@@ -1,10 +1,11 @@
 #include "headers/civilization.h"
 #include <iostream>
+#include <string>
 
 void Civilization::tick() {
     
 }
 
-std::string Civilization::getName() {
-    
+std::string Civilization::getName(const Civilization& Civ) {
+    return Civ.name; 
 }
