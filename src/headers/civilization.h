3a5bb886dff107117203de0c0d0cd75cd235a372
Filip Nachov <fnachov@gmail.com>
2025-07-03 12:39:04 +0200

    adding time

--- src/headers/civilization.h
+++ src/headers/civilization.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <string> 
+#include <format>
+#include <vector>
+
 
 class Civilization {
 private:
     std::string name;
+    std::vector<int> time = {0, 0, 0};
+    std::string fTime = std::format("time spent: {}s {}m {}h", time[0], time[1], time[2]);
     int population;
     int economy;
     double culture;
